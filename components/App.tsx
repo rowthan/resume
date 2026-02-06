e4fc7a1dc258f87903e04b744dc2d66666403785
rowthan <12638456+rowthan@users.noreply.github.com>
2026-02-06 21:06:37 +0800

    feat: custom 404 page

--- components/App.tsx
+++ components/App.tsx
@@ -5,30 +5,34 @@ import Education from "@/components/Education";
 import Jobs from "@/components/Jobs";
 import Projects from './Projects';
 import {AsideButton} from "@/components/Footer";
+import {notFound} from 'next/navigation';
 
 
-export default async function(props:{id: string}) {
+export default async function HomeApp(props: { id: string }) {
   const {id} = props
   let data;
-  if(!id){
+  if (!id) {
     data = await import('@/data/demo.json');
-  }else{
+  } else {
     const config = await (await fetch(`https://api.jsonbin.io/v3/b/${id}`)).json();
     data = config.record;
+    if (!data) {
+      return notFound();
+    }
   }
-  
-  return(
+
+  return (
     <div className={`root-container`}>
       <div className="resume-container">
-        <Profile profile={data.profile} />
-        <Skill skills={data.skills} />
-        <Jobs jobs = {data.jobs}/>
+        <Profile profile={data.profile}/>
+        <Skill skills={data.skills}/>
+        <Jobs jobs={data.jobs}/>
         <Projects projects={data.projects}/>
-        <Education education={data.education} />
+        <Education education={data.education}/>
         {/*<div className="other-section" style={{display:"flex",justifyContent:"space-between",flexWrap:"wrap"}}>*/}
         {/*</div>*/}
       </div>
-        <AsideButton/>
+      <AsideButton/>
       {/*{*/}
       {/*  this.state.revert &&*/}
       {/*  <Message/>*/}
